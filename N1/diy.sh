ceb7126e2da743bb454859fc79aeec94f74f9743
qpeng-lean <80767995+qpeng-lean@users.noreply.github.com>
2025-01-25 14:52:52 +0800

    Update diy.sh

--- N1/diy.sh
+++ N1/diy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Default IP
-sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.10.10/g' package/base-files/files/bin/config_generate
 
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 package/amlogic
